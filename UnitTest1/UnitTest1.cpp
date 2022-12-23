050548adf71e2f31d1ea5ec3cc193a89d0a2da76
yrii <yural1rn@gmail.com>
2022-12-23 09:02:27 +0200

    laba10.1

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba10.1/laba10.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char fname[100];
+			cout << "filename: "; cin >> fname;
+			cout << endl;
+			CreateTXT(fname);
+			PrintTXT(fname);
+			cout << endl;
+			cout << "k(,-) = " << ProcessTXT1(fname) << endl;
+			return ;
+		}
+	};
+}
